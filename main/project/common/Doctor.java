394e757d229b6da11ac16d3b3373fa0838504eb0
vadim khakham <vadim.khakham@gmail.com>
2017-01-21 22:14:01 +0200

    added class person

--- main/project/common/Doctor.java
+++ main/project/common/Doctor.java
@@ -2,45 +2,14 @@ package common;
 
 import java.io.Serializable;
 
-public class Doctor implements Serializable {
-    private Integer Id;
-    private String name;
+public class Doctor extends Person implements Serializable {
     private String branch;
     private String role;
 
     public Doctor(Integer id, String name, String branch, String role) {
-        Id = id;
-        this.name = name;
+        super(id, name);
         this.branch = branch;
         this.role = role;
-    }
-
-    public Doctor setId(Integer id) {
-        Id = id;
-        return this;
-    }
-
-    public Doctor setName(String name) {
-        this.name = name;
-        return this;
-    }
-
-    public Doctor setBranch(String branch) {
-        this.branch = branch;
-        return this;
-    }
-
-    public Doctor setRole(String role) {
-        this.role = role;
-        return this;
-    }
-
-    public Integer getId() {
-        return Id;
-    }
-
-    public String getName() {
-        return name;
     }
 
     public String getBranch() {
